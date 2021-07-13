96030a2f8ffb43c04018b3efcaf20ee082e715d9
JoePaul99 <73599839+JoePaul99@users.noreply.github.com>
2021-07-13 13:06:19 +0200

    Create helper.py
    User Specific Details

---
+++ helper.py
@@ -0,0 +1,10 @@
+# Zotero Details
+library_id = "..."
+library_type = "user"
+api_key = "..."
+
+
+# Notion Details 
+Notion_version = '2021-05-13'
+notion_url = 'https://api.notion.com/v1/databases/.../query' # Notion database url
+Notion_API_Key = "secret_..."
